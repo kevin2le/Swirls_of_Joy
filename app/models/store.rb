e52ca53f74c5a8a89cd1f6de95e29d4a5702091c
Kevin Le <captianle@gmail.com>
2017-07-28 16:54:50 -0700

    finals

--- app/models/store.rb
+++ app/models/store.rb
@@ -2,8 +2,8 @@ class Store < ApplicationRecord
     validates :name, presence: true, uniqueness: true
     validates :contact_info, presence:true
     validates :location, presence:true
-    has_many :comments
-    has_many :ratings
+    has_many :comments, dependent: :destroy 
+    has_many :ratings, dependent: :destroy 
 
 
 end
