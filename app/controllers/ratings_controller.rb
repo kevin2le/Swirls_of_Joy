3e511c3c568d080b4e670f896a1729ff25349aa6
Kevin Le <captianle@gmail.com>
2017-07-28 09:34:00 -0700

    final update 1.3

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -3,9 +3,6 @@ class RatingsController < ApplicationController
         store = Store.find(params[:store_id])
         rating = Rating.find_by(:user => current_user, :store => store)
         unless rating
-            puts '------------------------'
-            puts 'making a new rating'
-            puts '------------------------'
             store.ratings.create(user: current_user, rating: params[:rating])
         end
         redirect_to store_path(store)
