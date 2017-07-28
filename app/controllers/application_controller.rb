34afbf336dc191ed4222039e6ed3b8c01133aa31
Kevin Le <captianle@gmail.com>
2017-07-28 10:10:21 -0700

    final 3.0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,21 @@ class ApplicationController < ActionController::Base
       redirect_to login_path, alert:'unauthorized entry- you must be logged in' if current_user.nil?
     end
 
-
+    helper_method :average_rating
+    
+    def average_rating store 
+        store_id = store.id
+        rating = Rating.where(store_id: store_id)
+        sum = 0
+        rating.each do |rating|
+          sum += rating.rating
+        end
+        if rating.count == 0
+          0
+        else        
+          sum.to_f / rating.count
+        end
+      end
    
 
     
--- app/models/store.rb
+++ app/models/store.rb
@@ -4,18 +4,6 @@ class Store < ApplicationRecord
     validates :location, presence:true
     has_many :comments
     has_many :ratings
-    helper_method :average_rating
-    def average_rating
-        ratings = self.ratings
-        sum = 0
-        ratings.each do |rating|
-          sum += rating.rating
-        end
-        if ratings.count == 0
-          0
-        else        
-          sum.to_f / ratings.count
-        end
-     end
+
 
 end
